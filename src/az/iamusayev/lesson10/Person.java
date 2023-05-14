43a5ab46640d5547847f19b6a400b75df1eba2a3
Anar Musayev <anar.musayev@digirella.az>
2023-05-14 21:11:57 +0400

    Added lessons from 8 to 19

---
+++ src/az/iamusayev/lesson10/Person.java
@@ -0,0 +1,19 @@
+package az.iamusayev.lesson10;
+
+
+public class Person {
+
+    private final Integer id;
+
+    public Person(Integer id) {
+        this.id = id;
+    }
+
+    public boolean asBoolean() {
+        return id > 10;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+}
